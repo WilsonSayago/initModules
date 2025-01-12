252a6ec0d0af3da55f72e4bb3ceb06860605d2c3
WilsonSayago <sayagowilson92@gmail.com>
2025-01-11 22:13:45 -0300

    fix null pointer in create instance

--- initinstance.go
+++ initinstance.go
@@ -1,15 +1,11 @@
 package initModules
 
-import (
-	"sync"
-)
-
 var instances = make(map[string]interface{})
-var once sync.Once
 
 func GetInstance(key string, newInstance func() interface{}) interface{} {
-	once.Do(func() {
-		instances[key] = newInstance()
-	})
+	if instance, exists := instances[key]; exists {
+		return instance
+	}
+	instances[key] = newInstance()
 	return instances[key]
 }
